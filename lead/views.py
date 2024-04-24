84a4cdea7b2e77012982cacd0a5e6d02e109c5cd
ISDrozdov <isdrozdov@rustack.ru>
2024-04-24 21:07:38 +0300

    sanya asks

--- lead/views.py
+++ lead/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
 
-# Create your views here.
+from .forms import AddLeadForm
+
+@login_required
+def add_lead(request):
+    if request.method == 'POST':
+        form = AddLeadForm(request.POST)
+
+        if form.is_valid():
+            lead = form.save(commit=False)
+            lead.created_by = request.user
+            lead.save()
+
+            return redirect('dashboard') 
+    else:
+        form = AddLeadForm()
+
+    return render(request, 'lead/add_lead.html', {
+        'form': form
+    })
